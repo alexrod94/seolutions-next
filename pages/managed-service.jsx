eb04bd41137642e266a4e2f9e6aafb0f71607c82
alexrod94 <alexrod94@gmail.com>
2023-01-02 20:30:03 +0000

    Add picture in landing

--- pages/managed-service.jsx
+++ pages/managed-service.jsx
@@ -27,34 +27,43 @@ export default function ManagedService() {
         </div>
       </section>
       {/* Intro */}
-      <section className="mx-auto lg:w-5/6 py-24">
-        <p className="mb-4 font-medium text-xl">Dear friend,</p>
+      <section className="mx-auto px-4 py-24 flex flex-col lg:flex-row lg:items-center">
+        <div id="left-col" className="lg:w-5/6 lg:pr-2">
+          <p className="mb-4 font-medium text-xl">Dear friend,</p>
+          <p className="mb-4 font-medium text-xl">
+            Are you tired of struggling to generate leads for your SEO agency
+            through traditional means? Do you wish there was a way to reach
+            potential clients without the hassle of cold calling or the expense
+            of advertising?
+          </p>
 
-        <p className="mb-4 font-medium text-xl">
-          Are you tired of struggling to generate leads for your SEO agency
-          through traditional means? Do you wish there was a way to reach
-          potential clients without the hassle of cold calling or the expense of
-          advertising?
-        </p>
+          <p className="mb-4 font-medium text-xl">
+            At our agency, we specialize in generating leads for SEO agencies
+            through targeted cold email campaigns. With our expertise and
+            resources, we can help you tap into the vast potential of the
+            internet to grow your business.
+          </p>
 
-        <p className="mb-4 font-medium text-xl">
-          At our agency, we specialize in generating leads for SEO agencies
-          through targeted cold email campaigns. With our expertise and
-          resources, we can help you tap into the vast potential of the internet
-          to grow your business.
-        </p>
+          <p className="mb-4 font-medium text-xl">
+            Don't waste any more time and effort on ineffective lead generation
+            strategies. Let us handle the hard work for you, so you can focus on
+            what you do best - delivering top-notch SEO services to your
+            clients.
+          </p>
 
-        <p className="mb-4 font-medium text-xl">
-          Don't waste any more time and effort on ineffective lead generation
-          strategies. Let us handle the hard work for you, so you can focus on
-          what you do best - delivering top-notch SEO services to your clients.
-        </p>
-
-        <p className="mb-4 font-medium text-xl">
-          Imagine having a consistent flow of qualified leads coming in, without
-          the stress of trying to generate them yourself. With our lead
-          generation services, this can be a reality for your business.
-        </p>
+          <p className="mb-4 font-medium text-xl">
+            Imagine having a consistent flow of qualified leads coming in,
+            without the stress of trying to generate them yourself. With our
+            lead generation services, this can be a reality for your business.
+          </p>
+        </div>
+        <div id="right-col" className="lg:w-1/6">
+          <img
+            className="block mx-auto"
+            src="gradia-assets/images/landing/hero.jpeg"
+            alt=""
+          />
+        </div>
       </section>
     </>
   );
